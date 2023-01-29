87cd560044803196bbe209693f47c76a994ca68b
Zerthox <Zerthox@users.noreply.github.com>
2023-01-29 20:13:55 +0000

    deploy: db878f9c746339b203b9587409d6c983c51df099

--- implementors/arc_util/settings/trait.HasSettings.js
+++ implementors/arc_util/settings/trait.HasSettings.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["arc_util"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"arc_util":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
