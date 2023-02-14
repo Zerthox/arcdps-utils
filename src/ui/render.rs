d95beed21760b2699b990786a3de02bd662a1095
Zerthox <zerthox@gmail.com>
2023-02-14 17:12:45 +0100

    Add reset button

--- src/ui/render.rs
+++ src/ui/render.rs
@@ -20,6 +20,33 @@ pub fn small_padding<'ui>(ui: &'ui Ui) -> StyleStackToken<'ui> {
     ui.push_style_var(StyleVar::FramePadding([1.0, 1.0]))
 }
 
+/// Renders a reset button.
+pub fn reset_button(ui: &Ui, label: impl AsRef<str>, confirm: &mut bool) -> bool {
+    let mut changed = false;
+
+    if !*confirm {
+        if ui.button(label) {
+            *confirm = true;
+        }
+    } else {
+        ui.align_text_to_frame_padding();
+        ui.text(format!("{}?", label.as_ref()));
+
+        ui.same_line();
+        if ui.button("Confirm") {
+            changed = true;
+            *confirm = false;
+        }
+
+        ui.same_line_with_spacing(0.0, 5.0);
+        if ui.button("Cancel") {
+            *confirm = false;
+        }
+    }
+
+    changed
+}
+
 /// Renders a right-click context menu for the last item.
 pub fn item_context_menu(str_id: impl Into<String>, contents: impl FnOnce()) {
     if let Ok(str_id) = CString::new(str_id.into()) {
