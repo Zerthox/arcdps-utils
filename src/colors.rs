4f5de8a2b1e9bf7a732e10003e32e2ba0ef9599e
Zerthox <zerthox@gmail.com>
2023-05-31 22:06:11 +0200

    Add colors

---
+++ src/colors.rs
@@ -0,0 +1,41 @@
+pub type Color = [f32; 4];
+
+#[inline]
+pub const fn rgb(r: f32, g: f32, b: f32) -> Color {
+    [r, g, b, 1.0]
+}
+
+#[inline]
+pub const fn rgba(r: f32, g: f32, b: f32, a: f32) -> Color {
+    [r, g, b, a]
+}
+
+pub const BLACK: Color = rgb(0.0, 0.0, 0.0);
+
+pub const WHITE: Color = rgb(1.0, 1.0, 1.0);
+
+pub const RED: Color = rgb(1.0, 0.0, 0.0);
+
+pub const GREEN: Color = rgb(0.0, 1.0, 0.0);
+
+pub const BLUE: Color = rgb(0.0, 1.0, 0.0);
+
+pub const YELLOW: Color = rgb(1.0, 1.0, 0.0);
+
+pub const CYAN: Color = rgb(0.0, 1.0, 1.0);
+
+pub const MAGENTA: Color = rgb(1.0, 0.0, 1.0);
+
+pub const GREY: Color = rgb(0.5, 0.5, 0.5);
+
+pub const FINE: Color = rgb(0.384, 0.643, 0.855);
+
+pub const MASTERWORK: Color = rgb(0.102, 0.576, 0.024);
+
+pub const RARE: Color = rgb(0.988, 0.816, 0.043);
+
+pub const EXOTIC: Color = rgb(1.0, 0.643, 0.02);
+
+pub const ASCENDED: Color = rgb(0.984, 0.243, 0.553);
+
+pub const LEGENDARY: Color = rgb(0.592, 0.306, 1.0);
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! Utilities for interfacing with ArcDPS from Rust.
 
 pub mod api;
+pub mod colors;
 pub mod tracking;
 pub mod ui;
 pub mod util;
