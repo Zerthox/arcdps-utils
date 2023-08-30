3ace578805d4bac20d9dfa8db024b9a790b4deb3
Zerthox <zerthox@gmail.com>
2023-08-30 19:37:29 +0200

    Fix updater url

--- src/update/repo.rs
+++ src/update/repo.rs
@@ -4,7 +4,7 @@ use serde::Deserialize;
 /// Information about a GitHub repository.
 #[derive(Debug, Clone)]
 pub struct Repository {
-    url: String,
+    repo: String,
 }
 
 impl Repository {
@@ -13,28 +13,28 @@ impl Repository {
 
     /// Creates new repository information.
     #[inline]
-    pub fn new(owner: impl Into<String>, repo: impl Into<String>) -> Self {
+    pub fn new(owner: impl AsRef<str>, repo: impl AsRef<str>) -> Self {
         Self {
-            url: format!(
-                "https://api.github.com/repos/{}/{}/releases/latest",
-                owner.into(),
-                repo.into()
-            ),
+            repo: format!("{}/{}", owner.as_ref(), repo.as_ref()),
         }
     }
 
     /// Retrieves the latest version from the latest release tag.
     pub fn latest_version_blocking(&self) -> Option<Version> {
-        let res = minreq::get(&self.url)
-            .with_header("User-Agent", Self::AGENT)
-            .send();
-        let release = res.ok().and_then(|res| res.json::<Release>().ok());
-        release.and_then(|release| release.tag_name.parse().ok())
+        minreq::get(format!(
+            "https://api.github.com/repos/{}/releases/latest",
+            self.repo
+        ))
+        .with_header("User-Agent", Self::AGENT)
+        .send()
+        .ok()
+        .and_then(|res| res.json::<Release>().ok())
+        .and_then(|release| release.tag_name.parse().ok())
     }
 
     /// Opens the latest release.
     pub fn open_release(&self) {
-        let _ = open::that(&self.url);
+        let _ = open::that(format!("https://github.com/{}/releases/latest", self.repo));
     }
 }
 
